5f52b1d39821bd404974c7aed2eb28d4ef913514
Danny Lam <dannny.lam@gmail.com>
2026-03-15 13:25:06 +1100

    Add interactive prototype with Leaflet map and Figma capture support

---
+++ prototypes/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import PitchdLight from "./pitchd-light-v2.jsx";
+
+createRoot(document.getElementById("root")).render(<PitchdLight />);
